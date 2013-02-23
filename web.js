77002d3ea072fc2300b09e722ebbda1f0b2e4bdf
Odysseas Tsatalos <odysseas@tsatalos.com>
2013-02-23 14:20:54 -0800

    replaced connect with express

--- web.js
+++ web.js
@@ -1,9 +1,33 @@
 var port = process.env.PORT || 8081; 
+
+var express = require('express');
+var app = express()
+    .use(express.logger('dev'))
+    .use(express.favicon('public/favicon.ico'))
+    .use(express.static(__dirname + '/public'))
+    .use(express.errorHandler({dumpExceptions: true, showStack: true}))
+.listen(port);
+
+
+/*
+var connect = require('connect');
+var app = connect()
+    .use(connect.logger('dev'))
+    .use(connect.favicon('public/favicon.ico'))
+    .use(connect.static(__dirname + '/public'))
+    .use(connect.errorHandler({dumpExceptions: true, showStack: true}))
+.listen(port);
+*/
+
+
+/*
 var connect = require('connect');
 connect.createServer(
+    connect.logger('dev'),
     connect.favicon('public/favicon.ico'),
     connect.static(__dirname + '/public'),
-    connect.logger('dev')
+    connect.errorHandler({dumpExceptions: true, showStack: true})
 ).listen(port);
- 
+*/
+
 console.log('Server running at http://127.0.0.1:'+port+'/');
