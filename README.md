8d4ad660e6db470346c1e571523c04be1f0ce2b3
Odysseas Tsatalos <odysseas@tsatalos.com>
2013-03-02 08:54:27 -0800

    working version with register and logout

--- README.md
+++ README.md
@@ -2,5 +2,11 @@ Simplest possible hello world heroku node.js app that uses middleware and uses c
 
 - Here is the github repo : https://github.com/ogt/helloworld-node-cookies
 - Here is the heroku app : http://obscure-bastion-8852.herokuapp.com/  (20+ sec delay at first fetch)
-- Here is the blog post about this : http://otdump.blogspot.com/2013/02/work-notes-with-cookies.html
+- Here is the blog post about this : http://otdump.blogspot.com/2013/03/worknotes-helloworld-with-cookies.html
+
+Run locally as
+> foreman start -e .env_dev -f Procfile_dev
+> cat .env_dev 
+PORT=5000
+HOST=http://localhost:5000
 
--- web.js
+++ web.js
@@ -1,33 +1,59 @@
-var port = process.env.PORT || 8081; 
+require('./u.js')
+require('./nodeutil.js')
 
-var express = require('express');
-var app = express()
-    .use(express.logger('dev'))
-    .use(express.favicon('public/favicon.ico'))
-    .use(express.static(__dirname + '/public'))
-    .use(express.errorHandler({dumpExceptions: true, showStack: true}))
-.listen(port);
+_.run(function () {
+
+	var express = require('express');
+	var app = express();
+
+	app.use(express.logger('dev'));
+	app.use(express.favicon('public/favicon.ico'));
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+	app.use(express.cookieParser())
+	app.use(function (req, res, next) {
+		_.run(function () {
+			req.body = _.consume(req)
+		    next()
+		})
+	})
 
 
-/*
-var connect = require('connect');
-var app = connect()
-    .use(connect.logger('dev'))
-    .use(connect.favicon('public/favicon.ico'))
-    .use(connect.static(__dirname + '/public'))
-    .use(connect.errorHandler({dumpExceptions: true, showStack: true}))
-.listen(port);
-*/
+	app.get('/', function (req, res) {
+	    res.sendfile('./index.html')
+	});
 
 
-/*
-var connect = require('connect');
-connect.createServer(
-    connect.logger('dev'),
-    connect.favicon('public/favicon.ico'),
-    connect.static(__dirname + '/public'),
-    connect.errorHandler({dumpExceptions: true, showStack: true})
-).listen(port);
-*/
+	var fields = ['username','firstname','lastname']
+	app.all('/rpc', require('./rpc.js')({
+		getVersion : function () {
+			return 1
+		},
 
-console.log('Server running at http://127.0.0.1:'+port+'/');
+		register : function (arg, req, res) {
+			fields.forEach( function(field) {
+			    res.cookie(field, arg[field]);
+			})
+			return true;
+		},
+
+		logout : function(arg, req, res) {
+			fields.forEach( function(field) {
+				res.clearCookie(field)
+			})
+			return true;
+		},
+
+		getUser : function (arg, req, res) {
+			var userInfo = {}
+			fields.forEach( function(field) {
+				userInfo[field] = req.cookies[field]
+			})
+			return userInfo
+		}
+	}))
+
+	app.listen(process.env.PORT, function() {
+		console.log("go to " + process.env.HOST)
+	})
+})
